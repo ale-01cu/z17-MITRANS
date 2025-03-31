abad25b1df2e5431ae78aa27428f480bef4bb63f
ale-01cu <alejandro.figueroa.rodriguez.01@gmail.com>
2025-03-31 09:13:09 -0400

    Backend - Fix: websocket fixes

--- machine_learning/bot/main_2.py
+++ machine_learning/bot/main_2.py
@@ -1,7 +1,25 @@
 from bot import Bot
+import os
+from db.db import engine, Base
+
+# Crear todas las tablas en la base de datos
+Base.metadata.create_all(bind=engine)
+
+dirname = os.path.dirname(__file__)
+
+messenger_template_path = os.path.join(dirname, 'templates/messenger_template.png')
+messenger_template_2_path = os.path.join(dirname, 'templates/messenger_template_2.png')
 
 def main():
-    bot = Bot()
+    bot = Bot(
+        name='MessengerBot',
+        target_name='Messenger | Facebook',
+        target_templates_paths=[
+            messenger_template_path,
+            messenger_template_2_path
+        ],
+        websocket_uri="ws://your-server:port"
+    )
     bot.run()
 
 if __name__ == "__main__":
