0139be6aaa72e3f50367d59363591001e5bfb5ed
ale-01cu <alejandro.figueroa.rodriguez.01@gmail.com>
2025-04-14 09:47:55 -0400

    Backend - Fix: new comments api

--- backend/apps/comment/urls.py
+++ backend/apps/comment/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     CommentAPIView, GetCommentsFromExcelView,
     CreateCommentsView, ClassificationsByCommentsView,
-    UnreadCommentsListView, UrgentCommentsView
+    NewCommentsListView, UrgentCommentsView
 )
 from django.urls import path
 
@@ -13,6 +13,6 @@ urlpatterns = [
     path('upload/', GetCommentsFromExcelView.as_view(), name='get-comments-from-excel'),
     path('create-list/', CreateCommentsView.as_view(), name='create-comments-list'),
     path('classifications/', ClassificationsByCommentsView.as_view(), name='classifications-by-comments'),
-    path('unread/', UnreadCommentsListView.as_view(), name='unread-comments-list'),
+    path('unread/', NewCommentsListView.as_view(), name='unread-comments-list'),
     path('urgent/', UrgentCommentsView.as_view(), name='urgent-comments'),
 ] + router.urls
