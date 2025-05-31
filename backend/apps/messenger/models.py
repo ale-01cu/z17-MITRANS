6ce6f779b9d4bf894ea152b7a116aef84e5ba9cd
ale-01cu <alejandro.figueroa.rodriguez.01@gmail.com>
2025-05-31 00:32:23 -0400

    Fix: Message table deleted, comments and conversations tables updated

--- backend/apps/messenger/models.py
+++ backend/apps/messenger/models.py
@@ -3,7 +3,12 @@ from django.db import models
 # Create your models here.
 class Conversation(models.Model):
     # ID de messenger
-    id_messenger = models.AutoField(primary_key=True)
+    messenger_id = models.CharField(
+        max_length=50,
+        unique=True,
+        editable=False,
+        verbose_name="ID de Messenger",
+    )
 
     # ID del usuario con el que se ha iniciado la conversación
     user = models.ForeignKey(
@@ -18,39 +23,12 @@ class Conversation(models.Model):
     link = models.CharField(max_length=100)
 
     # Fecha de creación de la conversación en messenger
-    updated_time = models.DateTimeField(auto_now_add=True)
+    messenger_updated_at = models.DateTimeField(
+        verbose_name="Fecha de modificación en messenger"
+    )
 
     # Fecha de creación de la conversación en esta base de datos
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.user.username}'
-
-
-class Message(models.Model):
-    # ID de messenger
-    id_messenger = models.AutoField(primary_key=True)
-
-    # Relacion de 1 a muchos con la conversación
-    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
-
-    # Relacion de 1 a muchos con el usuario que envió el mensaje
-    sender = models.ForeignKey(
-        'comment_user_owner.UserOwner',
-        on_delete=models.CASCADE,
-        verbose_name="Usuario Propietario",
-        default=None,
-        null=True
-    )
-
-    # Texto del mensaje
-    content = models.TextField()
-
-    # Fecha de creacion del mensaje en messenger
-    created_time = models.DateTimeField(auto_now_add=True)
-
-    # Fecha de creacion del mensaje en esta base de datos
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.sender.username}: {self.content}'
